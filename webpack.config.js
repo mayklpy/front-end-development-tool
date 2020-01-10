45c9970cb67e10e3ca773967653bca6dc8a65ff2
mchlrtnz <mchlrtnz@gmail.com>
2020-01-10 12:07:51 -0500

    updated webpack.config.js to run webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require("path");
+const fse = require("fs-extra");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const postCSSPlugins = [];
 
+let pages = fse
+  .readdirSync("./app")
+  .filter(file => {
+    return file.endsWith(".html");
+  })
+  .map(page => {
+    return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./app/${page}`
+    });
+  });
+
 let config = {
-  entry: "./app/assets/scripts/index.js"
+  entry: "./app/assets/scripts/index.js",
+  plugins: pages
 };
 
 module.exports = config;
